3d3542dc1050881d1b730eb7de2d63dee09ada07
Carl Burks <crb02005@gmail.com>
2025-08-16 05:25:13 -0500

    cleanup

--- image-utilities/start.sh
+++
@@ -1,8 +0,0 @@
-
-
-#!/bin/bash
-
-echo "starting"
-#./watch_and_watermark.sh /home/bob/Desktop/watermarker /home/bob/Desktop/watermarker/watermark.png jpeg 10
-
-./watch_and_watermark.sh /mnt/c/users/crb02/Desktop/watermarker /mnt/c/users/crb02/Desktop/watermarker/watermark.png jpeg 10
